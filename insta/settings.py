f0f0ea298326feffbb94ada0e42162d53d592ac3
Lagom <jyyt0147@gmail.com>
2019-04-08 01:12:46 +0000

    posts 앱생성 and 설정

--- insta/settings.py
+++ insta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@p7*q+qn^4q1kbe@bwxcaw3lf7&gntu)c2%c%%dz!2hrqii*14'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
