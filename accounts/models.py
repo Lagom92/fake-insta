cf8c4c7520994f86755c79843771ac9f9decc49c
Lagom <jyyt0147@gmail.com>
2019-04-15 01:42:03 +0000

    signup 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="follower", blank=True)
+    # 위와 아래는 동일함
+    # follow = models.ManyToManyField('self', related_name="follower", blank=True)
+
     
     def __str__(self):
         return self.username
--- accounts/views.py
+++ accounts/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("posts:list")
         
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
 
     return render(request, 'accounts/form.html', {'form':form})
     
