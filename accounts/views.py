cc36a09e664ad59086e5be30754f55b752d49d87
Lagom <jyyt0147@gmail.com>
2019-04-16 01:13:40 +0000

    edit_profile

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def signup(request):
@@ -31,16 +33,18 @@ def login(request):
         
     return render(request, 'accounts/form.html', {'form':form})
     
-    
+@login_required
 def logout(request):
     auth_logout(request)
     return redirect("posts:list")
-    
+   
+@login_required
 def user_page(request, id):
     User = get_user_model()
     user_info = User.objects.get(id=id)
     return render(request, "accounts/user_page.html", {"user_info":user_info})
     
+@login_required
 def follow(request, id):
     # 로그인한 사람 
     me = request.user
@@ -57,3 +61,22 @@ def follow(request, id):
             me.followings.add(you)
     
     return redirect("accounts:user_page", id)
+    
+@login_required
+def edit_profile(request, id):
+    me = request.user
+    User = get_user_model()
+    user = User.objects.get(id=id)
+    if me == user:
+    
+        if request.method == "POST":
+            form = CustomUserChangeForm(request.POST, instance=user)
+            if form.is_valid():
+                form.save()
+                return redirect("accounts:user_page", id)
+        else:
+            form = CustomUserChangeForm(instance=user)
+        return render(request, 'accounts/form.html', {'form':form})
+    else:
+        return redirect("posts:list")
+    
